395f095120fe43e1a6209579dc6d25fe37c6ab42
Amanda Martinez Reyes <134201241+Amininor@users.noreply.github.com>
2023-05-25 16:15:37 +0200

    Create convert_temperature.py

---
+++ convert_temperature.py
@@ -0,0 +1,11 @@
+def fah_to_cel(temp_fahrenheit):
+        """
+        Converts temperature from fahrenheit to celsius
+        Parameters:
+        temp_fahrenheit
+
+        Returns:
+        temp_celsius
+        """
+        temp_celsius = (temp_fahrenheit -32) * 5/9
+        return temp_celsius
